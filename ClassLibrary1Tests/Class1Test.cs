b46cd4d450675ff5e3a82074f777c21f049b8fbd
William(Wu) Lee <xingmu33@gmail.com>
2016-01-18 07:03:02 -0800

    Initial commit

---
+++ ClassLibrary1Tests/Class1Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+
+namespace ClassLibrary1Tests
+{
+
+    [TestFixture]
+    public class Class1Test
+    {
+        [Test]
+        public void Function1_should_return_1()
+        {
+            // Arrange
+            var sut = new Class1();
+            // Act
+            var result = sut.Function1();
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+    }
+}
